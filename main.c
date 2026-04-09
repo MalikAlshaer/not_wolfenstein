881bbcf060161791e9adb22a4b929f8e8858a318
Furkan Ugur <151160685+Furkangr27-tr@users.noreply.github.com>
2026-04-09 20:53:51 +0300

    basic mouse controls by trigo functions

--- main.c
+++ main.c
@@ -25,18 +25,18 @@
 
 #define PLAYER_ROTATION_SPEED 1
 #define PLAYER_MOVEMENT_SPEED 1
-
+#define MOUSE_SPEED 0.1
 int map[MAP_HEIGHT][MAP_WIDTH] = {
-    {0,0,0,0,0,0,0,0,1,0},
-    {0,0,0,0,0,0,0,0,1,0},
-    {1,0,0,1,1,0,0,0,1,0},
+    {0,0,0,1,1,1,1,1,1,0},
+    {0,0,0,1,0,0,0,0,1,0},
+    {1,0,0,1,1,0,1,0,1,0},
     {1,0,0,0,1,0,0,0,1,0},
+    {1,0,0,0,1,0,0,0,1,0},
+    {1,0,1,1,1,0,0,0,1,0},
+    {1,0,1,0,0,0,0,0,1,0},
+    {1,0,1,1,1,1,1,0,1,0},
     {1,0,0,0,0,0,0,0,1,0},
-    {1,0,0,0,0,0,0,0,1,0},
-    {1,0,0,0,0,0,0,0,1,0},
-    {1,0,0,0,0,0,0,0,1,0},
-    {1,0,0,0,0,0,0,0,1,0},
-    {1,1,1,1,1,0,0,1,1,0}
+    {1,1,1,1,1,1,1,1,1,0}
 };
 
 typedef struct {
@@ -47,6 +47,7 @@ Player player = {0, 0, 45};
 
 void DrawCoords(){ //DrawRectangle(player.x, player.y, 10, 10, PLAYER_COLOR);
     DrawText(TextFormat("x:%.2f y:%.2f", player.x, player.y), 10, 10, 20, RED);
+    //DrawText(TextFormat())
 }
 
 // returns the distance to next wall assuming the player looks in given direction
@@ -95,7 +96,7 @@ void DrawVerticalLine(double height, double x){
     // what?
     double y = HEIGHT/2.0 - height/2;
 
-    if(vertical_rect.x > 250 && y > 250){
+    if(vertical_rect.x > 250 && y > 350 && vertical_rect.x < 400 &&y < 600){
         DrawRectangleRec(vertical_rect, WHITE);
     }
 
@@ -125,9 +126,14 @@ void DrawFOV(){
 void move(){
     double temp_x = player.x;
     double temp_y = player.y;
-
+    float angleX = 0,angleY = 0;
+    Vector2 mouseDelta = GetMouseDelta();
+    angleX -= mouseDelta.y * MOUSE_SPEED;
+    angleY += mouseDelta.x * MOUSE_SPEED;
+    //player.angle += cos(angleY) * 0.1;
+    player.angle +=sin(angleY * DEG2RAD) * 50;
     if(IsKeyDown(KEY_D)) player.angle += PLAYER_ROTATION_SPEED;
-
+    
     if(IsKeyDown(KEY_A)) player.angle -= PLAYER_ROTATION_SPEED;
 
     if(IsKeyDown(KEY_W)){
