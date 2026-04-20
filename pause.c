9a7f1b2b8f755dae419da3c08ccefce904a5889d
Furkan Ugur <151160685+FurkanUgrr@users.noreply.github.com>
2026-04-20 15:55:02 +0300

    game paused text
    Added a DrawText call to display 'GAME PAUSED' on the menu.

--- pause.c
+++ pause.c
@@ -1,5 +1,9 @@
 #include "raylib.h"
+//fonkisyon çalıştırma
 
 void DrawMenu(int width, int height, Texture2D texture) {
+    
     DrawTexture(texture, width/2 - texture.width/2, height/2 - texture.height/2, RAYWHITE);
+    DrawText("GAME PAUSED",width/2 - 98,height/2 - 190,30,WHITE);
+    //buraya yaz
 }
