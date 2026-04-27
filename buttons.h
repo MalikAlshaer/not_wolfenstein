0fb4072bc1da68afd66c8cee1aa5e11a05b22977
Malik Alshaer <malik.h.alshaer@gmail.com>
2026-04-27 18:54:03 +0300

    Declared more functions

--- buttons.h
+++ buttons.h
@@ -1,8 +1,16 @@
 #ifndef BUTTONS_H_
 #define BUTTONS_H_
 
-void ResumePause(void);
+void PauseGame(void);
 
-void VolumeOnOff(void);
+void MusicOnOff(void);
+
+void IncMusicVolume(void);
+
+void DecMusicVolume(void);
+
+void IncMouseSens(void);
+
+void DecMouseSens(void);
 
 #endif
